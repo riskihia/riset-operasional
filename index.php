a1e5b770481dbd98604eda144773b7bdd7ccb798
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-10-04 17:31:10 +0700

    tampil halaman form

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Linear Programming</title>
+    <link rel="stylesheet" href="./css/output.css">
+</head>
+<body>
+    <h1 class="text-3xl text-center">Riset Operasional : Linear programming</h1>
+    
+    <div class="container px-10">
+        <form action="">
+            
+            <div class="my-5">
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <div class="mt-2 md:w-1/3">
+                    <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>                
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Installation
+
+Install semua dependency (tailwind)
+
+```
+npm install
+```
+
+run :
+
+```
+npx tailwindcss -i ./css/style.css -o ./public/css/output.css --watch
+```
